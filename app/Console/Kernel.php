def08db666f0fe6ca8a95b58a3a38a88547af94e
Jade <zhengyiunity@gmail.com>
2022-03-22 14:25:51 +0800

    Feat: 安装数据库备份插件 spatie/laravel-backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,4 +1,14 @@
 <?php
+/*
+ * @Author: Jadedever
+ * @Date: 2022-02-08 23:52:58
+ * @LastEditors: Jadedever
+ * @LastEditTime: 2022-03-22 14:17:17
+ * @FilePath: /laravel9init/app/Console/Kernel.php
+ * @Description:
+ *
+ * Copyright (c) 2022 by Jadedever, All Rights Reserved.
+ */
 
 namespace App\Console;
 
@@ -16,6 +26,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        // 备份
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30');
     }
 
     /**
@@ -25,7 +39,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
